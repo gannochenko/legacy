28c19e5779f80ccdeb03ba23ccddc717b57146f6
Sergei <gannochenko.sv@gmail.com>
2019-03-05 20:24:20 +0100

    tmp folder removed

--- container-list/script/push.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-VERSION="${1:-latest}"
-
-docker build -t awesome1888/container-list:$VERSION -f docker/production.dockerfile .;
-docker push awesome1888/container-list:$VERSION
-# docker rmi awesome1888/container-list:$VERSION
