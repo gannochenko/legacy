1646f24e225d04a29ec10d08a92922110cdb6101
Sergei <gannochenko.sv@gmail.com>
2019-03-05 21:51:23 +0100

    thoughts

--- app.api/src/lib/db-diff.js
+++ app.api/src/lib/db-diff.js
@@ -21,15 +21,45 @@ export default class DBDiff {
             tables = await qr.getTables(eTableNames);
         }
 
+        console.dir('Tables');
         tables.forEach(table =>
             console.log(require('util').inspect(table.columns, { depth: 10 })),
         );
 
         // get all entites
-        const list = await entityProvider.get();
-        console.dir(list);
+        const entities = await entityProvider.get();
+        console.dir('Entities');
+        console.log(require('util').inspect(entities, { depth: 10 }));
 
-        // now we need to move entity fields to the same declaration and make diff
-        // ...
+        // unify the format between a tables and entities:
+
+        // calculate diff:
+
+        // tables to be created
+        // // for each reference field create a connection table
+
+        // tables to be dropped
+        // // for each reference field - don't forget to drop the corresponding connection table
+
+        // tables to be updated (the most of fun is here)
+        // for each table to update:
+
+        // columns to be added
+        // // for each reference field create a connection table
+
+        // columns to be removed
+        // // for each reference field - don't forget to drop the corresponding connection table
+
+        // columns to be updated in terms of type
+        // // if the field was a reference field, but became something else - drop the connection table
+        // // if the field was something else, but became a reference field - add a connection table
+        // // if the field was a reference field, and stays the same, but gets connected to the other entity, then drop one connection table and create another (or should not we?)
+
+        // Thoughts:
+        // the field is not allowed to be renamed, because the field name is it's "unique" id. Besides, there is no typical case for renaming fields, only damn perfectionists do so.
+        // the name of the connection table shall be eg_ref_<entity-name>_<field-name>
+        // in postgres the maximum length of an identifier is 63, so we can allow <entity-name> and <field-name> to be no more than 27 characters long
+        // this is not good. we can replace these identifiers with hashes or meaningless aliases
+        // for the first version I guess I am not going to allow field type change
     }
 }
