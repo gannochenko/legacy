8e547af334da38e880fdefa348ecf940d4bcd395
awesome <awesome1888@gmail.com>
2019-04-04 22:12:58 +0200

    links

---
+++ shared/constants.js
@@ -0,0 +1,18 @@
+export const DB_TABLE_PREFIX = 'eq_';
+export const DB_REF_TABLE_PREFIX = `${DB_TABLE_PREFIX}ref_`;
+export const DB_ENTITY_TABLE_PREFIX = `${DB_TABLE_PREFIX}e_`;
+export const DB_MIGRATION_TABLE_NAME = `${DB_TABLE_PREFIX}migrations`;
+export const DB_SCHEMA_TABLE_NAME = `${DB_TABLE_PREFIX}schema`;
+export const DB_IDENTIFIER_LENGTH = 63;
+export const DB_VARCHAR_DEF_LENGTH = 255;
+export const DB_CODE_COLUMN_LENGTH = 36;
+export const QUERY_FIND_MAX_PAGE_SIZE = 50;
+
+export const ENTITY_USER_NAME = 'user';
+export const ENTITY_GROUP_NAME = 'group';
+export const ENTITY_CODE_FIELD_NAME = 'code';
+
+export const ENTITY_TYPE_STRING = String;
+export const ENTITY_TYPE_NUMBER = Number;
+export const ENTITY_TYPE_BOOLEAN = Boolean;
+export const ENTITY_TYPE_DATE = Date;
---
+++ shared/schema/field.js
@@ -0,0 +1,91 @@
+import { DB_VARCHAR_DEF_LENGTH } from '../../constants';
+
+export const TYPE_STRING = 'string';
+export const TYPE_INTEGER = 'integer';
+export const TYPE_BOOLEAN = 'boolean';
+export const TYPE_DATETIME = 'datetime';
+
+export default class Field {
+    constructor(declaration) {
+        if (!_.ione(declaration)) {
+            declaration = {};
+        }
+        this._schema = declaration;
+    }
+
+    checkHealth() {
+        const errors = [];
+        const schema = this._schema;
+
+        // check that field has at least name and type
+        if (!_.isne(schema.name)) {
+            errors.push({
+                message: 'Field does not have a name',
+                code: 'field_name_empty',
+                reference: null,
+            });
+        }
+
+        if (!_.isne(this.getActualType())) {
+            errors.push({
+                message: 'Field does not have a type',
+                code: 'field_type_empty',
+                reference: schema.name,
+            });
+        }
+
+        return errors;
+    }
+
+    isMultiple() {
+        return _.isArray(this._schema.type);
+    }
+
+    getType() {
+        return this._schema.type || null;
+    }
+
+    getActualType() {
+        const type = this.getType();
+        if (!type) {
+            return null;
+        }
+
+        return this.isMultiple() ? type[0] : type;
+    }
+
+    getLength() {
+        const type = this.getType();
+        if (type === TYPE_STRING) {
+            const length = parseInt(this._schema.length, 10);
+            if (isNaN(length)) {
+                return DB_VARCHAR_DEF_LENGTH;
+            }
+
+            return length;
+        }
+
+        return null;
+    }
+
+    getName() {
+        return this._schema.name;
+    }
+
+    getReferenceFieldName() {
+        const type = this.getActualType();
+        if (
+            [TYPE_STRING, TYPE_BOOLEAN, TYPE_DATETIME, TYPE_INTEGER].indexOf(
+                type,
+            ) >= 0
+        ) {
+            return null;
+        }
+
+        return type;
+    }
+
+    toJSON() {
+        return this._schema;
+    }
+}
