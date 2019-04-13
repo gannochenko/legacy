2bf421caaf0ac5028bcf898254eda38d6e04f326
awesome <awesome1888@gmail.com>
2019-04-13 23:33:10 +0200

    checkpoint

---
+++ app.admin/common/components/DatePicker/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { DatePicker } from './style.js';
+
+export default ({ children }) => <DatePicker />;
---
+++ app.admin/common/components/DatePicker/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+// import {
+//   align
+// } from 'sc-companion';
+// import {
+// 	withTheme,
+// 	stdLink,
+// } from '../../style/global';
+
+export const DatePicker = styled.div``;
