4ff074309cc7a0fb37b25692525a5d112911a447
awesome <awesome1888@gmail.com>
2019-05-25 22:48:35 +0200

    need to check something

---
+++ app.api/src/lib/field.js
@@ -0,0 +1,166 @@
+/* eslint import/no-unresolved: 0 */
+
+import { uCFirst } from 'ew-internals';
+import * as yup from 'yup';
+import { DB_VARCHAR_DEF_LENGTH } from 'project-minimum-core';
+import {
+    TYPE_STRING,
+    TYPE_BOOLEAN,
+    TYPE_DATETIME,
+    TYPE_INTEGER,
+} from 'project-minimum-core';
+
+export class Field {
+    constructor(declaration) {
+        if (!_.ione(declaration)) {
+            declaration = {};
+        }
+        this.schema = {
+            ...declaration.name,
+            ...declaration.type,
+            ...declaration.label,
+            ...declaration.length,
+            ...declaration.required,
+            ...declaration.unique,
+            ...declaration.preview,
+        };
+    }
+
+    checkHealth() {
+        const errors = [];
+        const { schema } = this;
+        const { name, type, label, length, required, unique, preview } = schema;
+
+        // use yup here...
+
+        // check that field has name
+        if (!_.isne(name)) {
+            errors.push({
+                message: 'Field does not have a name',
+                code: 'field_name_empty',
+                reference: null,
+            });
+        }
+
+        if (!_.isne(type) && !(_.iane(type) && _.isne(type[0]))) {
+            errors.push({
+                message:
+                    'Type should be a string or an array of one string element',
+                code: 'field_type_illegal',
+                reference: schema.name,
+            });
+        }
+
+        if (!_.isne(this.getActualType())) {
+            errors.push({
+                message: 'Field does not have a type',
+                code: 'field_type_empty',
+                reference: schema.name,
+            });
+        }
+
+        return errors;
+    }
+
+    getFieldValidator() {
+        if (!this.fieldValidator) {
+            this.fieldValidator = {
+                name: yup
+                    .string('Field name should be a string')
+                    .required('Field should have a name'),
+                type: yup
+                    .oneOf([yup.string(), yup.array().shape()])
+                    .required('Field should have a type'),
+                label: yup.string('Field label should be a string'),
+                length: yup.number('Field length should be a number'),
+                required: yup.boolean('Field required flag should be boolean'),
+                unique: yup.boolean('Field unique flag should be boolean'),
+                preview: yup.boolean('Field preview flag should be boolean'),
+            };
+        }
+
+        return this.fieldValidator;
+    }
+
+    getType() {
+        return this.schema.type || null;
+    }
+
+    getActualType() {
+        const type = this.getType();
+        if (!type) {
+            return null;
+        }
+
+        return this.isMultiple() ? type[0] : type;
+    }
+
+    getLength() {
+        const type = this.getType();
+        if (type === TYPE_STRING) {
+            const length = parseInt(this.schema.length, 10);
+            if (Number.isNaN(length)) {
+                return DB_VARCHAR_DEF_LENGTH;
+            }
+
+            return length;
+        }
+
+        return null;
+    }
+
+    getName() {
+        return this.schema.name;
+    }
+
+    getDisplayName() {
+        return _.isne(this.schema.label)
+            ? this.schema.label
+            : uCFirst(this.getName()).replace(/_/g, ' ');
+    }
+
+    getReferencedEntityName() {
+        const type = this.getActualType();
+        if (
+            [TYPE_STRING, TYPE_BOOLEAN, TYPE_DATETIME, TYPE_INTEGER].indexOf(
+                type,
+            ) >= 0
+        ) {
+            return null;
+        }
+
+        return type;
+    }
+
+    /**
+     * @deprecated
+     * @returns {*}
+     */
+    getReferenceFieldName() {
+        return this.getReferencedEntityName();
+    }
+
+    isMultiple() {
+        return _.isArray(this.schema.type);
+    }
+
+    isReference() {
+        return _.isne(this.getReferencedEntityName());
+    }
+
+    isSortable() {
+        return !this.isMultiple() && !this.isReference();
+    }
+
+    isMandatory() {
+        return this.schema.required === true;
+    }
+
+    isPreview() {
+        return this.schema.preview === true;
+    }
+
+    toJSON() {
+        return this.schema;
+    }
+}
---
+++ app.api/src/schema.test.js
@@ -0,0 +1,11 @@
+describe('Schema', () => {
+    beforeAll(() => {
+        console.dir('Before!');
+    });
+    beforeEach(() => {
+        console.dir('Before each');
+    });
+    it('should work', () => {
+        console.dir('lala');
+    });
+});
