7f30f21b1a068f00084746932bdccf5068081e23
awesome <awesome1888@gmail.com>
2019-06-13 21:56:10 +0200

    body bg color

--- app.admin.mobile/App.js
+++ app.admin.mobile/App.js
@@ -1,38 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View, WebView } from 'react-native';
-import Swipeable from 'react-native-gesture-handler/Swipeable';
+// import Swipeable from 'react-native-gesture-handler/Swipeable';
 
-export default class App extends React.Component {
+class App extends React.Component {
     // https://kmagiera.github.io/react-native-gesture-handler/docs/component-swipeable.html
     render() {
-        return (
-            <Swipeable
-                style={{ flex: 1 }}
-                renderRightActions={(progress, dragX) => {
-                    return <Text>"I am here!"</Text>;
-                }}
-            >
-                <Text>
-                    <View style={{ flex: 1 }}>
-                        <View
-                            style={{ flex: 1, backgroundColor: 'powderblue' }}
-                        />
-                        <View style={{ flex: 2, backgroundColor: 'skyblue' }} />
-                        <View
-                            style={{ flex: 3, backgroundColor: 'steelblue' }}
-                        />
-                    </View>
-                </Text>
-            </Swipeable>
-        );
-
-        //return <WebView source={{ uri: 'http://192.168.178.23:3000/' }} />;
+        return <Text>Some text</Text>;
 
         // return (
-        //   <View style={styles.container}>
-        //     <Text>!Open up App.js to start working on your app!</Text>
-        //   </View>
+        //     <Swipeable
+        //         style={{ flex: 1 }}
+        //         renderRightActions={(progress, dragX) => {
+        //             return <Text>"I am here!"</Text>;
+        //         }}
+        //     >
+        //         <Text>
+        //             <View style={{ flex: 1 }}>
+        //                 <View
+        //                     style={{ flex: 1, backgroundColor: 'powderblue' }}
+        //                 />
+        //                 <View style={{ flex: 2, backgroundColor: 'skyblue' }} />
+        //                 <View
+        //                     style={{ flex: 3, backgroundColor: 'steelblue' }}
+        //                 />
+        //             </View>
+        //         </Text>
+        //     </Swipeable>
         // );
+
+        //return <WebView source={{ uri: 'http://192.168.178.23:3000/' }} />;
     }
 }
 
@@ -47,3 +43,5 @@ const styles = StyleSheet.create({
         flex: 1,
     },
 });
+
+export default App;
--- app.admin/common/style/theme.js
+++ app.admin/common/style/theme.js
@@ -24,7 +24,7 @@ const theme = {
     body: {
         color: palette.black,
         background: {
-            color: palette.white,
+            color: '#fafafa',
         },
     },
     link: {
