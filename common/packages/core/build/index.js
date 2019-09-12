25f7af377e762bb4b093f56a60b34f0298c67b85
awesome <awesome1888@gmail.com>
2019-09-12 21:04:45 +0200

    another build

--- common/packages/core/build/index.js
+++ common/packages/core/build/index.js
@@ -1,9 +1,10 @@
-"use strict";
+'use strict';
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./constants.both"));
-__export(require("./constants.server"));
-__export(require("./schema"));
+Object.defineProperty(exports, '__esModule', { value: true });
+__export(require('./constants.both'));
+__export(require('./constants.server'));
+__export(require('./schema/field/field-type'));
+__export(require('./schema'));
 //# sourceMappingURL=index.js.map
