7b1fa8d5e8cbcf02f7a3b7460a62f771bf878422
awesome <awesome1888@gmail.com>
2019-09-13 09:10:24 +0200

    install peers

--- common/script/release.sh
+++ common/script/release.sh
@@ -6,6 +6,12 @@ then
     exit 1;
 fi
 
+yarn run install-peers;
+if ! [ $? -eq 0 ]
+then
+    exit 1;
+fi
+
 yarn run test;
 if ! [ $? -eq 0 ]
 then
@@ -18,7 +24,7 @@ then
     exit 1;
 fi
 
-#git add -A ./;
-#git commit -m "another build";
-#
-#yarn lerna publish --contents build --npm-client=yarn --no-git-reset;
+git add -A ./;
+git commit -m "another build";
+
+yarn lerna publish --contents build --npm-client=yarn --no-git-reset;
