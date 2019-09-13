b6063b5be24ef4a3b8e357241416c88fc279eacb
awesome <awesome1888@gmail.com>
2019-09-13 20:55:52 +0200

    reset

--- common/script/reset.sh
+++ common/script/reset.sh
@@ -4,3 +4,6 @@ sed -i 's/1\.0\.1/1\.0\.0/g' lerna.json;
 sed -i 's/"1\.0\.1/"1\.0\.0/g' packages/core/package.json;
 git tag -d v1.0.1;
 git push origin :refs/tags/v1.0.1;
+git add -A ./
+git commit -am "reset"
+git push
