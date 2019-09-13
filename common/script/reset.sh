795faff6c3996fcfaa1c0c2eccfcb2326708206b
awesome <awesome1888@gmail.com>
2019-09-13 20:43:31 +0200

    reset

--- common/script/reset.sh
+++ common/script/reset.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sed -i 's/1\.0\.0/0\.0\.0/g' lerna.json;
-sed -i 's/"1\.0\.0/"0\.0\.0/g' packages/core/package.json;
+sed -i 's/1\.0\.1/1\.0\.0/g' lerna.json;
+sed -i 's/"1\.0\.1/"1\.0\.0/g' packages/core/package.json;
 git tag -d v1.0.1;
 git push origin :refs/tags/v1.0.1;
