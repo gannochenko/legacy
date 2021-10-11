e5499f5103908ddc1faa66e6842e00011283efdf
Sergei <gannochenko.sv@gmail.com>
2021-10-11 23:20:11 +0200

    11

--- apps/prussiascan.api/script/deploy.sh
+++ apps/prussiascan.api/script/deploy.sh
@@ -14,14 +14,14 @@ then
   export $(cat "${ENV}" | xargs)
 fi
 
-#aws s3api create-bucket --bucket="${S3_BUCKET}" --region="${S3_REGION}" --create-bucket-configuration LocationConstraint="${S3_REGION}"
+aws s3api create-bucket --bucket="${S3_BUCKET}" --region="${S3_REGION}" --create-bucket-configuration LocationConstraint="${S3_REGION}"
 
 # build
 yarn || exit
 yarn webpack --config webpack.build.config.js --mode production --entry ./src/lambdas/"${FN_NAME}"/handler.js -o ./build/lambdas/"${FN_NAME}"/ || exit
 cd ./build/lambdas/"${FN_NAME}"
 zip ./"${ZIP}" main.js
-#
-## deploy
-#aws s3 cp ./"${ZIP}" s3://"${S3_BUCKET}"/"${S3_KEY}"
-#aws lambda update-function-code --function-name "${FN_NAME_GLOBAL}" --s3-bucket "${S3_BUCKET}" --s3-key "${S3_KEY}" --region="${S3_REGION}"
+
+# deploy
+aws s3 cp ./"${ZIP}" s3://"${S3_BUCKET}"/"${S3_KEY}"
+aws lambda update-function-code --function-name "${FN_NAME_GLOBAL}" --s3-bucket "${S3_BUCKET}" --s3-key "${S3_KEY}" --region="${S3_REGION}"
